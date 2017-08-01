faf7fd164807155f9ecf913905abb25594b5e250
Fred Morrison <fpmorrison@verizon.net>
2017-07-31 21:24:02 -0400

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+
+@NgModule({
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		NgbModule.forRoot(),
+		AppRoutingModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
+})
+export class AppModule {
+}

c6b8d7f64cee51ab978b29b4a493967352c15232
Fred Morrison <fpmorrison@verizon.net>
2020-04-03 17:43:24 -0400

    use navtive ngx-bootstrap

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -2,20 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { AngularVersionInformationService } from '../shared/angular-version-information.service';
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+	selector: 'app-footer',
+	templateUrl: './footer.component.html',
+	styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
-  private compName: string = 'FooterComponent';
-  public angularVersion: string;
+	private compName = 'FooterComponent';
+	public angularVersion: string;
 
-  constructor(private ngVersionInfo: AngularVersionInformationService) {
-    console.log(`${this.compName} - constructor`);
-  }
+	constructor(private ngVersionInfo: AngularVersionInformationService) {
+		console.log(`${this.compName} - constructor`);
+	}
 
-  ngOnInit() {
-    this.angularVersion = this.ngVersionInfo.versionFull;
-    console.log(`${this.compName} - ngOninit - version=${this.angularVersion}`);
-  }
+	ngOnInit() {
+		this.angularVersion = this.ngVersionInfo.versionFull;
+		console.log(`${this.compName} - ngOninit - version=${this.angularVersion}`);
+	}
 }
